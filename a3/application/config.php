d82405633f0ac6a74e4f79af7875e75ba064ff9f
ClaytonSagar <31390704+ClaytonSagar@users.noreply.github.com>
2017-10-20 17:31:56 -0400

    a3

---
+++ a3/application/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:21951/a3/');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'csagar');
+define('DB_PASS', 'coolclayton1');
+define('DB_NAME', 'csagar_db');
---
+++ a3/application/controllers/controller.php
@@ -0,0 +1,48 @@
+<?php
+
+class Controller {
+	
+   	public $load;
+		public $data = array();
+
+
+	function __construct($view, $method = null, $parameters = null){
+				$this->load = new Load();
+				new Model();
+				if($method){
+					$this->runTask($method, $parameters);
+				}else{
+					$this->defaultTask();
+				}
+				$this->load->view($view.'.php', $this->data);
+	}
+
+	public function runTask($method, $parameters = null){
+		
+		if($method && method_exists($this, $method)) {
+			 		
+					//the call_user_func_array expects an array so we create a null array if parameters is empty
+					if(!is_array($parameters)){
+						$parameters = array();
+					}
+		
+          call_user_func_array(array($this, $method), $parameters); 
+		  
+     	}
+	
+	}
+	
+
+	public function defaultTask(){
+	
+	}
+	
+	public function set($key, $value){
+		
+		$this->data[$key] = $value;
+		
+	}
+
+
+
+}
