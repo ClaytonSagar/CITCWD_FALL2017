606afb26dc89fcb4070ea0dfcf56986aa3620b22
ClaytonSagar <31390704+ClaytonSagar@users.noreply.github.com>
2017-10-23 00:06:52 -0400

    ex4

---
+++ ex4/application/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DEFAULT_VIEW', 'home');
+define('BASE_URL', 'http://corsair.cs.iupui.edu:21951/CIT31300/FALL2017/ex4/');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'csagar');
+define('DB_PASS', 'coolclayton1');
+define('DB_NAME', 'csagar_db');
